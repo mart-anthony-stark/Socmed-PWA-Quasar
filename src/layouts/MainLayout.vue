9b080bafe011547181a1572c18135d3b6863470f
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-28 21:15:23 +0800

    add list

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,9 +8,101 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+    <q-drawer
+      class="q-pa-sm"
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+    >
       <!-- left drawer content -->
       <q-icon name="favorite" size="lg" color="primary" />
+
+      <q-list class="q-mt-sm">
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon color="primary" name="bluetooth" />
+          </q-item-section>
+
+          <q-item-section>Icon as avatar</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar color="teal" text-color="white" icon="bluetooth" />
+          </q-item-section>
+
+          <q-item-section>Avatar-type icon</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar
+              rounded
+              color="purple"
+              text-color="white"
+              icon="bluetooth"
+            />
+          </q-item-section>
+
+          <q-item-section>Rounded avatar-type icon</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar color="primary" text-color="white"> R </q-avatar>
+          </q-item-section>
+
+          <q-item-section>Letter avatar-type</q-item-section>
+        </q-item>
+
+        <q-separator />
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Image avatar</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar square>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Image square avatar</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar rounded>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>Image rounded avatar</q-item-section>
+        </q-item>
+
+        <q-separator />
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar rounded>
+              <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>List item</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section thumbnail>
+            <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+          </q-item-section>
+          <q-item-section>List item</q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
