569056b6a3fc64d68dba38c7190e33bbfd3f9ddd
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-28 21:34:43 +0800

    add q item

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -60,6 +60,21 @@
           <q-icon name="search" />
         </template>
       </q-input>
+
+      <q-item>
+        <q-item-section>
+          <q-item-label overline>OVERLINE</q-item-label>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption
+            >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+            adipiscit elit.</q-item-label
+          >
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
     </q-drawer>
 
     <q-page-container>
